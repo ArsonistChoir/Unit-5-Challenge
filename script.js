e1b5d01520db7728e9e00040a2dfc9fbb3fdc5c1
ArsonistChoir <haiden2004ellis@outlook.com>
2023-12-06 19:42:41 -0600

    Added saving functionality, started on adding the rest of the times

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+$(document).ready(function () {
 
-  $( document ).ready(function() {
-  $(".saveBtn").on("click", function() {
-    let hour = $(this).parent().attr("id")
-  let text = $(this).siblings(".description").val();
-  localStorage.setItem(hour, text);
-  })
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  $ ("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $(".saveBtn").on("click", function () {
+    let hour = $(this).parent().attr("id");
+    let text = $(this).siblings(".description").val();
+    localStorage.setItem(hour, text);
+  });
+
+  function updateHourClasses() {
+    var currentHour = dayjs().hour();
+
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      if (blockHour < currentHour) {
+        $(this).addClass("past").removeClass("present future");
+      } else if (blockHour === currentHour) {
+        $(this).addClass("present").removeClass("past future");
+      } else {
+        $(this).addClass("future").removeClass("past present");
+      }
+    });
+  }
+
+  function loadSavedDescriptions() {
+    $(".time-block").each(function () {
+      var hour = $(this).attr("id");
+      var savedText = localStorage.getItem(hour);
+      $(this).find(".description").val(savedText);
+    });
+  }
+
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+
+  updateHourClasses();
+  loadSavedDescriptions();
+
+  setInterval(function () {
+    updateHourClasses();
+  }, 60000);
 });
-
-// Psuedo Code
-
-//Display today's date via  DayJS
-
-//Need to add more hours/timeblocks for input in your HTML
-
-//Functionality for the SaveButton
-//Add event listneer, need to be able to store and remove (clears) user input fron local storage
-
-//Create some sort of Timer Function
- //loop through each time block... based upon current time it will know if it is past present or future
- 
-//On page reload... any input in local storage needs to be rendered to the correct hour
